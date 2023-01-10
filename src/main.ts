710e84b5f2f2fa3293e5f81eb137a66b77816cf4
alex <arhontes@yandex.ru>
2023-01-10 22:08:15 +0300

    refactor(main.ts): set up cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ const port = process.env.PORT || 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
   await app.listen(port);
 }
 bootstrap();
