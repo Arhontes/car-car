39af7564223c881bb201e8ce1a2e875eaef773fe
alex <arhontes@yandex.ru>
2023-01-12 22:08:00 +0300

    feature(validation-types.ts): change refresh-jwt.guard

--- src/modules/auth/guards/refresh-jwt.guard.ts
+++ src/modules/auth/guards/refresh-jwt.guard.ts
@@ -1,6 +1,8 @@
 import {
   CanActivate,
   ExecutionContext,
+  HttpException,
+  HttpStatus,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -13,27 +15,39 @@ export class RefreshJWTGuard implements CanActivate {
 
   async canActivate(
     context: ExecutionContext,
+
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
 
     const token = request.cookies['token'];
-    const { phone } = request.body;
     if (!token) {
       throw new UnauthorizedException('Поле refresh_token обязательно');
     }
-    if (!phone) {
-      throw new UnauthorizedException(`Поле phone обязательно`);
+
+    const validToken = await this.authService.verifyToken(token);
+
+    if (!validToken.error || validToken?.error === 'jwt expired') {
+      const parseData = this.authService.parseJwt(token);
+
+      const user = this.authService.validateUser({ userId: parseData.userId });
+      if (!user) {
+        throw new UnauthorizedException(
+          'Пользователя c таким именем не существует',
+        );
+      }
+      request.userId = parseData.userId;
     }
 
-    const user = await this.authService.validateUser({ phone });
-
-    if (!user) {
-      throw new UnauthorizedException(
-        'Пользователя c таким именем не существует',
-      );
+    if (validToken?.error) {
+      if (validToken.error === 'jwt expired') {
+        return (request.isFreshToken = false);
+      } else {
+        throw new HttpException(validToken.error, HttpStatus.BAD_REQUEST);
+      }
+    } else {
+      return (request.isFreshToken = true);
     }
-    return true;
   }
 }
