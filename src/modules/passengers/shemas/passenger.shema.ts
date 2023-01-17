c43368e46339d222233d82a9bffab13a50590fb6
alex <arhontes@yandex.ru>
2023-01-17 20:35:29 +0300

    feature(passengers.module): add passengers module

---
+++ src/modules/passengers/shemas/passenger.shema.ts
@@ -0,0 +1,37 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+export type PassengerDocument = Passenger & Document;
+
+@Schema()
+export class Passenger {
+  @Prop({ type: String, required: true })
+  phone;
+
+  @Prop({ type: String, required: true, default: () => uuidv4() })
+  userId;
+
+  @Prop({ type: String, required: true })
+  email;
+
+  @Prop({ type: String, required: true })
+  from;
+
+  @Prop({ type: String, required: true })
+  to;
+
+  @Prop({ type: String, required: true })
+  firstName;
+
+  @Prop({ type: String, required: true })
+  lastName;
+
+  @Prop({ type: Boolean, default: false })
+  approved;
+
+  @Prop({ type: String, default: '0', required: false })
+  reservedTime;
+}
+
+export const PassengerSchema = SchemaFactory.createForClass(Passenger);
