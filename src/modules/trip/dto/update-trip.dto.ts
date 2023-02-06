146183eee335d8a3d64b03d185df639fb46a2354
alex <arhontes@yandex.ru>
2023-02-06 20:07:24 +0300

    feature(update-trip.dto.ts): refactor code and add property

--- src/modules/trip/dto/update-trip.dto.ts
+++ src/modules/trip/dto/update-trip.dto.ts
@@ -3,8 +3,7 @@ import { Car } from '../../car/shemas/car.shema';
 export class UpdateTripDto {
   readonly direction: string;
   readonly date: number;
+  readonly startTime: string;
   readonly car: Car;
-  /*readonly passengers: Array<Passenger> | null;*/
-  readonly tripId: string;
-  readonly managerId: string;
+  readonly userId: string;
 }
