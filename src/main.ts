3dce7c22d294ae14a46c189c84b19d3ac7ac7774
alex <arhontes@yandex.ru>
2023-02-22 15:32:21 +0300

    fix cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app/app.module';
-import * as cookieParser from 'cookie-parser';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./modules/app/app.module";
+import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 5000;
 
@@ -8,10 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true,
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true
   });
   await app.listen(port);
 }
+
 bootstrap();
