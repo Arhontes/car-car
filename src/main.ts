c3f538d37a5dc89b245206648fb62bb1c222838a
alex <arhontes@yandex.ru>
2023-02-27 11:47:30 +0300

    refactor(main.ts): refactor cors settings

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./modules/app/app.module";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app/app.module';
 import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 5000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:false});
+  const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  /*app.enableCors({
-    origin: "https://onega-go.vercel.app",
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true
-  });*/
+  app.enableCors({
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
   await app.listen(port);
 }
 
